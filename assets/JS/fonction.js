ad21f8d2188d8c4f7c7e61ea67fe4f10b7f931b3
fadikamohamed <fadika.mohamed02@gmail.com>
2018-06-23 11:04:12 +0200

    Ajout modif addEventListener

--- assets/JS/fonction.js
+++ assets/JS/fonction.js
@@ -1,4 +1,6 @@
+f1 = document.getElementById('lastname');
+f1.addEventListener('focusout',focusout);
 
 function focusout() {
-  alert('Merci de votre participation')
+  alert('Merci de votre participation');
 }
